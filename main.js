a44a4830b7c608eaaaac63f2496af5e8afd96904
fyhaa <124665072+fyhaa1010@users.noreply.github.com>
2024-05-05 23:39:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+// let myAmain=["Ahmed","osama,","sayed","samer"];
+// let myEmploye=["Amgad","samah","Amerr","Omer","Osman","Aman","Smah"];
+// let x=3;
+// document.write(`we have ${x} Admin`)
+// document.write("<hr>")
+
+// for(let i=0;i<x;i++){
+//     document.write(`<div>The admin of teame ${i + 1} is ${myAmain[i]}</div>`)
+//     document.write(`<h3>the meambers of team:</h3>`)
+//     document.write(`<hr>`)
+//     for(let j=i;j<myEmploye.length;j++){
+//       if(myEmploye[j].startsWith("A")){
+//         document.write(`<p>${myEmploye[j]}</p>`)
+//       }
+     
+      
+      
+// }
+
+// }
+
+// let header=document.createElement("header");
+// let head=document.createElement("a")
+// let ull=document.createElement("ul")
+// let l1=document.createElement("li")
+// let l2=document.createElement("li")
+// let l3=document.createElement("li")
+// let di1=document.createElement("div")
+// let di2=document.createElement("div")
+// let spa=document.createElement("span")
+
+
+
+
+// let x=document.createTextNode("fyhaa")
+// let y=document.createTextNode("home")
+// let z=document.createTextNode("home")
+// let z2=document.createTextNode("home")
+// let di22=document.createTextNode("product")
+// let xx=document.createTextNode(1)
+
+
+
+
+
+
+// head.appendChild(x);
+// header.appendChild(head);
+// l1.appendChild(y)
+// ull.appendChild(l1)
+// header.appendChild(ull)
+// l2.appendChild(z)
+// ull.appendChild(l2)
+// l3.appendChild(z2)
+// ull.appendChild(l3)
+
+// di1.appendChild(di2)
+// di2.appendChild(di22)
+// di2.appendChild(spa)
+// spa.appendChild(xx)
+// header.className="heaader";
+// di1.className="content";
+// di2.className="x";
+// header.style.width="80%";
+// header.style.padding="10px";
+// header.style.margin="10px";
+// header.style.display="flex";
+// header.style.justifyContent="space-between";
+// header.style.alignItems="center";
+// ull.style.display="flex";
+// ull.style.justifyContent="space-between";
+// ull.style.alignItems="center";
+// ull.style.textDecoration="none";
+// ull.style.paddingLeft="10px"
+// di1.style.border="2px solid";
+// di1.style.width="10%";
+// di1.style.padding="5px";
+// di1.style.display="flex",
+// di1.style.justifyContent="center";
+// di1.style.flexWrap="warp";
+// di1.style.paddingBottom="10px";
+// di1.style.paddingTop="10px";
+// di1.style.gap="20px";
+// di1.style.minHeight="calc(100vh-140px)";
+// di1.style.boxSizing="border-box"
+
+
+
+
+
+
+
+
+// document.body.appendChild(header);
+// document.body.appendChild(di1)
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $("h2").fadeIn(3000,function(){
+        $("p").fadeIn(6000)
+    })
+})
